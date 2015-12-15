6b2ebb7a19a477e1e635a6426fed3260e56b084f
Brian Woodward <brian.woodward@gmail.com>
2015-12-15 12:02:16 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * sort-prompt <https://github.com/doowb/sort-prompt>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
